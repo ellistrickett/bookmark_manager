76b92609136f96334e4e931cbeaeea7bf031b914
Ellis Trickett <ellis.trickett@gmail.com>
2020-05-05 16:01:18 +0100

    Set up testing environment

--- README.md
+++ README.md
@@ -5,8 +5,15 @@ As a user,
 so I can see my bookmarks,
 show me a list of bookmarks.
 
+  ## bookmark_manager Database
 
   1.  Connect to psql
   2.  Create the database using the psql command CREATE DATABASE  bookmark_manager;
   3.  Connect to the database using the pqsl command \c bookmark_manager;
   4.  Run the query we have saved in the file 01_create_bookmarks_table.sql
+
+  ## Test Database
+
+  1.  Connect to psql
+  2.  Create the database using the psql command CREATE DATABASE  bookmark_manager_test;
+  3.  Connect to the database using the pqsl command \c bookmark_manager_test;
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,14 +1,25 @@
-
-feature 'shows hello world on page' do
-  scenario 'when user opens page see text hello world' do
-    visit('/')
-    expect(page).to have_content("Bookmark Manager")
+feature 'Viewing bookmarks' do
+  feature 'visiting the homepage' do
+    scenario 'the page title is visible' do
+      visit '/'
+      expect(page).to have_content 'Bookmark Manager'
+    end
   end
-end
 
-feature 'shows bookmarks at the /bookmarks route' do
-  scenario 'user visits /bookmarks and sees bookmarks' do
-    visit('/bookmarks')
-    expect(page).to have_content("http://www.google.com")
+  feature 'viewing bookmarks' do
+    scenario 'bookmarks are visible' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      # Add the test data
+      connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
+      visit '/bookmarks'
+
+      expect(page).to have_content 'http://www.makersacademy.com'
+      expect(page).to have_content 'http://www.destroyallsoftware.com'
+      expect(page).to have_content 'http://www.google.com'
+    end
   end
 end
